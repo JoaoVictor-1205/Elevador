28a81bf60286aa40e70cb37830b2f289cdedfee9
JoaoVictor-1205 <JoaoVictor-1205@users.noreply.github.com>
2026-03-20 19:52:44 -0300

    fix: ajusta tamanho do diagrama UML

--- scripts/gerar_diagrama_uml.ps1
+++ scripts/gerar_diagrama_uml.ps1
@@ -3,15 +3,27 @@ Add-Type -AssemblyName System.Drawing
 $outputPath = Join-Path $PSScriptRoot "..\docs\diagrama-uml-elevador.png"
 $outputPath = [System.IO.Path]::GetFullPath($outputPath)
 
+$attributes = @(
+    "- andarMinimo: int",
+    "- andarMaximo: int",
+    "- andarAtual: int",
+    "- portaAberta: boolean"
+)
+
+$methods = @(
+    "+ Elevador(andarMinimo: int, andarMaximo: int)",
+    "+ subir(): boolean",
+    "+ descer(): boolean",
+    "+ abrirPorta(): boolean",
+    "+ fecharPorta(): boolean",
+    "+ isPortaAberta(): boolean",
+    "+ getAndarAtual(): int",
+    "+ getAndarMinimo(): int",
+    "+ getAndarMaximo(): int",
+    "+ getStatusPorta(): String"
+)
+
 $width = 1400
-$height = 900
-
-$bitmap = New-Object System.Drawing.Bitmap $width, $height
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-
-$graphics.Clear([System.Drawing.Color]::White)
-$graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
-$graphics.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::ClearTypeGridFit
 
 $borderPen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(30, 41, 59)), 3
 $dividerPen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(100, 116, 139)), 2
@@ -32,19 +44,31 @@ $noteFont = New-Object System.Drawing.Font("Arial", 15, [System.Drawing.FontStyl
 $classX = 120
 $classY = 90
 $classWidth = 760
-$classHeight = 700
 $headerHeight = 90
-$attributesHeight = 220
+$sectionContentOffset = 55
+$rowHeight = 38
+$sectionBottomPadding = 45
+$attributesHeight = $sectionContentOffset + (($attributes.Count - 1) * $rowHeight) + $sectionBottomPadding
+$methodsHeight = $sectionContentOffset + (($methods.Count - 1) * $rowHeight) + $sectionBottomPadding
+$classHeight = $headerHeight + $attributesHeight + $methodsHeight
+$height = [Math]::Max(900, $classY + $classHeight + 50)
+
+$bitmap = New-Object System.Drawing.Bitmap $width, $height
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+
+$graphics.Clear([System.Drawing.Color]::White)
+$graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
+$graphics.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::ClearTypeGridFit
 
 $headerRect = New-Object System.Drawing.Rectangle $classX, $classY, $classWidth, $headerHeight
 $classRect = New-Object System.Drawing.Rectangle $classX, $classY, $classWidth, $classHeight
 
+$line1Y = $classY + $headerHeight
+$line2Y = $line1Y + $attributesHeight
+
 $graphics.FillRectangle($classBackground, $classRect)
 $graphics.FillRectangle($headerBackground, $headerRect)
 $graphics.DrawRectangle($borderPen, $classRect)
-
-$line1Y = $classY + $headerHeight
-$line2Y = $line1Y + $attributesHeight
 $graphics.DrawLine($dividerPen, $classX, $line1Y, $classX + $classWidth, $line1Y)
 $graphics.DrawLine($dividerPen, $classX, $line2Y, $classX + $classWidth, $line2Y)
 
@@ -56,26 +80,6 @@ $graphics.DrawString("Elevador", $titleFont, $textBrush, [System.Drawing.Rectang
 
 $graphics.DrawString("Atributos", $sectionFont, $textBrush, 150, ($line1Y + 15))
 $graphics.DrawString("Metodos", $sectionFont, $textBrush, 150, ($line2Y + 15))
-
-$attributes = @(
-    "- andarMinimo: int",
-    "- andarMaximo: int",
-    "- andarAtual: int",
-    "- portaAberta: boolean"
-)
-
-$methods = @(
-    "+ Elevador(andarMinimo: int, andarMaximo: int)",
-    "+ subir(): boolean",
-    "+ descer(): boolean",
-    "+ abrirPorta(): boolean",
-    "+ fecharPorta(): boolean",
-    "+ isPortaAberta(): boolean",
-    "+ getAndarAtual(): int",
-    "+ getAndarMinimo(): int",
-    "+ getAndarMaximo(): int",
-    "+ getStatusPorta(): String"
-)
 
 $attributeStartY = $line1Y + 55
 for ($i = 0; $i -lt $attributes.Count; $i++) {
